a2c246805f42c9e0514b4e351f19e9b30f5e83ea
ENSEMUQAC <pierremasquilier@gmail.com>
2017-11-20 09:58:08 -0500

    premier commit

---
+++ FingerPath.java
@@ -0,0 +1,32 @@
+package paintlike.com.paintlike;
+
+/**
+ * Created by Pierre on 20/11/2017.
+ */
+
+
+import android.graphics.Path;
+
+
+public class FingerPath {
+
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+
+
+
+}
+
+
+
